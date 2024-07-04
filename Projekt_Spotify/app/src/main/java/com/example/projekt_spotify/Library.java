98bc41555d2b7806b168dc4d41ebaffb55f27e81
Mikołaj Janczak <mikolaj.janczaq@gmail.com>
2024-07-04 13:46:04 +0200

    Przechodzenie miedzy aktywnosciami---- poprawiono wyswietlanie nav bara

--- Projekt_Spotify/app/src/main/java/com/example/projekt_spotify/Library.java
+++ Projekt_Spotify/app/src/main/java/com/example/projekt_spotify/Library.java
@@ -1,11 +1,16 @@
 package com.example.projekt_spotify;
 
+import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP;
+
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Library extends AppCompatActivity {
 
@@ -14,10 +19,25 @@ public class Library extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_library);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.library), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        overridePendingTransition(R.anim.none, R.anim.none);
+
+        BottomNavigationView bottomNav = findViewById(R.id.nav_view);
+
+        bottomNav.setSelectedItemId(R.id.navigation_library);
+
+        bottomNav.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    startActivity(new Intent(this, MainActivity.class));
+                    break;
+                case R.id.navigation_search:
+                    startActivity(new Intent(this, Search.class).addFlags(FLAG_ACTIVITY_CLEAR_TOP));
+                    break;
+                case R.id.navigation_library:
+                    startActivity(new Intent(this, Library.class).addFlags(FLAG_ACTIVITY_CLEAR_TOP));
+                    break;
+            }
+            return true;
         });
     }
 }
--- Projekt_Spotify/app/src/main/java/com/example/projekt_spotify/MainActivity.java
+++ Projekt_Spotify/app/src/main/java/com/example/projekt_spotify/MainActivity.java
@@ -1,16 +1,13 @@
 package com.example.projekt_spotify;
 
+import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,10 +16,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        overridePendingTransition(R.anim.none, R.anim.none);
+
+        BottomNavigationView bottomNav = findViewById(R.id.nav_view);
+
+        bottomNav.setSelectedItemId(R.id.navigation_home);
+
+        bottomNav.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    startActivity(new Intent(this, MainActivity.class));
+                    break;
+                case R.id.navigation_search:
+                    startActivity(new Intent(this, Search.class).addFlags(FLAG_ACTIVITY_CLEAR_TOP));
+                    break;
+                case R.id.navigation_library:
+                    startActivity(new Intent(this, Library.class).addFlags(FLAG_ACTIVITY_CLEAR_TOP));
+                    break;
+            }
+            return true;
         });
     }
 }
